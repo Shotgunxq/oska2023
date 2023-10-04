55df6aec8ef17e7ac66e779f386d084439200f76
Bence Bodnar <bodnar.bence001@gmail.com>
2023-10-04 14:57:24 +0200

    feat: sys trace not working

--- kernel/proc.c
+++ kernel/proc.c
@@ -322,6 +322,8 @@ fork(void)
   np->state = RUNNABLE;
   release(&np->lock);
 
+  np->mask = p->mask; //This line is new, copy mask
+
   return pid;
 }
 
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -91,3 +91,14 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_trace(void) {
+  int trace_mask;
+  if(argint(0, &trace_mask) < 0) {
+    return -1;
+  }
+  struct proc* p = myproc();
+  p->mask = trace_mask;
+  return 0;
+}
